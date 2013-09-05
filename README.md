b6d2b50ce0ba54a6d799554bf7cdd4df6041ff9a
Brett Zamir <brettz9@yahoo.com>
2013-09-05 18:46:43 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-jhtml
-=====
+# JHTML
+
+The JHTML format is a strict subset of HTML used to encode arbitrary JSON within HTML. This library seeks to provide conversions while simultaneously validating the indicated JHTML structure(s).
+
+Possible use cases include:
+
+1. Hierarchical data storage in a faithful, readily portable and readily viewable format.
+1. Allow building of data files within (schema-constrained) WYSIWYG editors
+
+JHTML ought to be round-trippable with canonical JSON except in the case when converting *from* object-containing JSON to JHTML when the ECMAScript/JSON interpreter does not iterate the properties in definition order (as ECMAScript interpreters are not obliged to do).
+
+# Rules
+
+Currently, comment (and processing instructions) and whitespace text nodes are allowed throughout, but any elements must be constrained to the expected types. For canonicalization, attributes beyond those explicitly allowed should not be present. Microdata might not care about hierarchy, but this specification adds such constraints.
+
+1. A top-level JSON string primitive will be encoded by the presence of `<span>` without an `itemprop` attribute, and whose contents will be stringified into JSON upon serialization.
+1. Other top-level JSON primitives (null, boolean, or number) will be encoded by `<span itemprop="{type}">` where `{type}` is the case-sensitive value of `null`, `boolean`, or `number`, and whose contents will be valid JSON.
+1. JSON arrays (in whatever context) willl be encoded as `<ol start="0">` whose individual child items (if any) will be represented by `<li itemprop="{type}">` where `type` follows the same conventions as with top-level primitives except that the absence of an `itemprop` attribute along with a single `<dl>` or `<ol>` child will indicate a child object or array respectively (a string can still be indicated by the absence of an `itemprop` attribute if the `<li>` has no children).
+1. JSON objects (in whatever context) will be encoded as `<dl>` whose individual child items (if any) will be represented by alternating `<dt>`/`<dd>` pairs (only single instances are allowed for each within a pair). `<dt>` will represent the keys of the object, whereas `<dd>` will represent the values. `<dd itemprop="{type}">` where `type` follows the same conventions as with top-level primitives except that the absence of an `itemprop` attribute along with a single `<dl>` or `<ol>` child will indicate a child object or array respectively (a string can still be indicated by the absence of an `itemprop` attribute if the `<dd>` has no children).
+
+# Usage
+
+```html
+<script src="jhtml.js"></script>
+```
+
+```javascript
+// The following code will look for all elements within the document belonging to the JHTML itemtype namespace (currently `http://brett-zamir.me/ns/microdata/json-as-html/1`).
+// Alternatively, one may supply the items as the first (and only) argument (there is no validation for namespace currently
+in such a case).
+// These return a JSON array if multiple elements are found or a single object otherwise
+JHTML.toJSONObject(); // returns a JSON object
+JHTML.toJSONString(); // returns a JSON string
+```
+
+If you intend to support older browsers, you will need polyfills for:
+
+1. Array.prototype.map
+1. Array.prototype.reduce
+1. Element.prototype.textContent
+1. Element.prototype.itemProp
+1. HTMLDocument.prototype.getItems
+
+# Possible future todos
+1. Implement JHTML.toJHTMLDOM()
+1. Implement JHTML.toJHTMLString()
+1. Define as ECMAScript 6 Module with polyfill plug-in
+1. Allow equivalents to JSON.parse's reviver or JSON.stringify's replacer and space arguments? 
+
+# Possible future spec additions
+
+The following might perhaps be allowed in conjunction with [JSON Schema](http://json-schema.org/), although I would also like to allow optional encoding of non-JSON JavaScript objects as well.
+
+1. This could be expanded to support types like: undefined, function, URL, date, etc.
+1. Support a special HTML-aware string type to allow arbitrary nested HTML where JSON strings are expected (which might be encapsulated say by a <div itemprop="html">). This could still convert to JSON, but as a string.
+1. Could use itemid/itemref to encode linked references
+
+# Possible future spec modifications
+
+The following may loosen requirements, but may not be desirable as they would allow expansion of the size of JHTML files.
+
+1. Loosen requirements to always use `<ol start="0">`? For portable proper structural readability, however, this seems like it should stay, even though CSS can mimic the correct 0-indexed display.
+1. Loosen requirements to allow `<span>` on primitives within object keys or object or array keys or values. Currently, the shortest possible expression is required?
+1. Loosen requirements to allow an explicit itemprop="string", itemprop="array", and/or itemprop="object"?
+1. Require primitives to be within <data> elements (but the HTML spec currently requires a `value` attribute which would be redundant with the human-readable value so <span> is being used instead)
+
+The following are possible tightening changes:
+1. Disallow comment and processing instruction nodes? Despite the precedent with JSON disallowing comments, I am partial to allowing comment nodes in JHTML, despite the burden on implementers, as it is extremely convenient to be able to include such information within data files. Of course, they will not be round-trippable with JSON (unless encoded as a legitimate part of the JSON object) since JSON disallows comments.
+
+The following are other possible changes:
+
+1. Change the itemtype namespace
+1. Allow multiple <dd>'s if taken to mean array children? (Probably more confusing even if more succinct than requiring a child <ol>)
+1. Anything else that comes up out of consultation with others (although I intend to change the namespace upon any breaking changes)
+
+# Inspiration
+
+JHTML was inspired by Netscape bookmark files as used when exporting bookmarks in Firefox. They brought to my attention that `<dl>` could be used to represent nestable key-value data hierarchies as also found in JSON objects.
---
+++ jhtml.js
@@ -0,0 +1,154 @@
+/*globals exports*/
+/**
+* JHTML is a format which can represent arbitrary JSON structures in a faithful, human-readable, and portable manner.
+* It is also round-trippable except in the case when converting *from* object-containing JSON to JHTML when the
+* ECMAScript/JSON interpreter does not iterate the properties in definition order (as it is not required to do)
+* @namespace Contains methods for conversions between JSON and JHTML (as strings or objects)
+* @requires polyfill: Array.prototype.map
+* @requires polyfill: Array.prototype.reduce
+* @requires polyfill: Element.prototype.textContent
+* @requires polyfill: Element.prototype.itemProp
+* @requires polyfill: HTMLDocument.prototype.getItems
+* @todo Finish unimplemented methods
+* @todo Add polyfills, e.g., https://github.com/termi/Microdata-JS/
+*/
+(function () {'use strict';
+    function ignoreHarmlessNonelementNodes (node, item) {
+        if (
+            (node.nodeType === 3 || node.nodeType === 4) && // Text or CDATA node
+            !node.nodeValue.match(/^\s*$/)
+        ) {
+            throw 'Non-whitespace text or CDATA nodes are not allowed directly within <' +
+                            item.nodeName.toLowerCase() + '>';
+        }
+        // Todo: also ignore nodes like comments or processing instructions? (A mistake of JSON?); we might even convert comments into JavaScript comments if this is used in a non-JSON-restricted JavaScript environment
+        return node.nodeType !== 1; // Not an element (ignore comments, whitespace text nodes, etc.)
+    }
+    function getJSONFromItemProp (item) {
+        var textContent = item.textContent,
+            itemProp = item.itemProp.toString(); // FF for some reason doesn't convert to a string; item.getAttribute('itemprop'); // 
+        switch(itemProp) {
+            case 'null':
+                if (textContent !== 'null') {
+                    throw 'Invalid null value';
+                }
+                break;
+            case 'boolean':
+                if (textContent !== 'true' && textContent !== 'false') {
+                    throw 'Invalid boolean value';
+                }
+                break;
+            case 'number':
+                if (!textContent.match(/^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:e(?:\+|-)?\d+)?$/i)) {
+                    throw 'Invalid number value';
+                }
+                break;
+            // case 'string': // Todo: Allow?
+            // case null: // Todo: Use this with getAttribute() but not .itemProp
+            case '': // Todo: Use this with .itemProp.toString() but not with with .itemProp
+            case undefined: // string is the default
+                return JSON.stringify(item.textContent);
+            default:
+                throw 'Unrecognized itemprop value: ' + itemProp;
+        }
+        return textContent;
+    }
+    function item2JSONString (item, throwOnSpan) {
+        function getHarmlessChildNodes (node) {
+            return function (prev, childNode) {
+                if (ignoreHarmlessNonelementNodes(childNode, node)) {
+                    return prev;
+                }
+                // As per check above, will be exactly one element node in this batch
+                return prev + item2JSONString(childNode, true);
+            };
+        }
+        var ret, state, topLevelJSONElement = item.nodeName.toLowerCase();
+        switch (topLevelJSONElement) {
+            case 'span':
+                if (throwOnSpan) {
+                    throw 'A <span> element is not allowed in this context';
+                }
+                return getJSONFromItemProp(item);
+            case 'dl': // object
+                // JSON allows empty objects (and HTML allows empty <dl>'s) so we do also
+                state = 'dt';
+                ret = [].reduce.call(item.childNodes, function (prev, node) {
+                    if (ignoreHarmlessNonelementNodes(node, item)) {
+                        return prev;
+                    }
+                    var nodeName = node.nodeName.toLowerCase();
+                    if (state !== nodeName) {
+                        throw 'Unexpected state encountered: ' + state;
+                    }
+                    if (nodeName === 'dt') {
+                        if (node.children.length) {
+                            throw '<dt> should not have any children';
+                        }
+                        state = 'dd';
+                        return prev + JSON.stringify(node.textContent) + ':';
+                    }
+                    // Can now only be a <dd>
+                    state = 'dt';
+                    if (node.children.length > 1) {
+                        throw '<dd> should not have more than one child';
+                    }
+                    if (!node.children.length) {
+                        return prev + getJSONFromItemProp(node) + ',';
+                    }
+                    return prev + [].reduce.call(node.childNodes, getHarmlessChildNodes(node), '') + ',';
+                }, '{').replace(/,$/, '') + '}';
+                if (state !== 'dt') {
+                    throw 'Ended a definition list without a final <dd> to match the previous <dt>.';
+                }
+                return ret;
+            case 'ol': // array
+                if (item.getAttribute('start') !== '0') {
+                    throw 'For the sake of readability, <ol> must include a start="0" attribute';
+                }
+                // JSON allows empty arrays (and HTML allows empty <ol>'s) so we do also
+                return [].reduce.call(item.childNodes, function (prev, node) {
+                    if (ignoreHarmlessNonelementNodes(node, item)) {
+                        return prev;
+                    }
+                    var nodeName = node.nodeName.toLowerCase();
+                    if (nodeName !== 'li') {
+                        throw 'Unexpected child of <ol> element: ' + nodeName;
+                    }
+                    if (node.children.length > 1) {
+                        throw '<li> should not have more than a single <ol> or <dl> child';
+                    }
+                    if (!node.children.length) {
+                        return prev + getJSONFromItemProp(node) + ',';
+                    }
+                    return prev + node.childNodes.reduce(getHarmlessChildNodes(node), '') + ',';
+                }, '[').replace(/,$/, '') + ']';
+        }
+    }
+    var exp;
+    if (typeof exports === 'undefined') {
+        window.JHTML = {};
+        exp = window.JHTML;
+    }
+    else {
+        exp = exports;
+    }
+
+    exp.toJSONObject = function (items) {
+        return JSON.parse(this.toJSONString(items));
+    };
+    /**
+    * We don't validate that other attributes are not present, but they should not be
+    */
+    exp.toJSONString = function (items) {
+        var jsonHtml = items || document.getItems('http://brett-zamir.me/ns/microdata/json-as-html/1'),
+            ret = [].map.call(jsonHtml, item2JSONString);
+        return ret.length === 1 ? ret[0] : '[' + ret.join(',') + ']';
+    };
+    exp.toJHTMLDOM = function () {
+        throw 'Not yet implemented';
+    };
+    exp.toJHTMLString = function () {
+        throw 'Not yet implemented';
+    };
+}());
